9e68dee0ec37e084f22eea1eb4a894d16576e6cf
KarMaPlaYzz <36639598+KarMaPlaYzz@users.noreply.github.com>
2025-09-10 19:32:44 +0200

    Update styles and social links in chat widget

--- chat-widget.js
+++ chat-widget.js
@@ -155,6 +155,15 @@
             align-items: center;
             flex-wrap: wrap;
             justify-content: space-between;
+            border-radius: 8px;
+            padding: 5px 14px 5px 14px;
+            background-color: rgba(0, 0, 0, 0);
+            transition: background-color 0.2s ease-in-out;
+        }
+
+        .n8n-chat-widget .new-conversation-socials .social a:hover {
+            background-color: rgba(0, 0, 0, 0.1);
+            transition: background-color 0.2s ease-in-out;
         }
 
         .n8n-chat-widget .new-conversation-socials .social .app-icon {
@@ -187,7 +196,7 @@
             background: linear-gradient(135deg, var(--chat--color-primary) 0%, var(--chat--color-secondary) 100%);
             color: white;
             border: none;
-            border-radius: 9999px;
+            border-radius: 10px;
             cursor: pointer;
             font-size: 16px;
             transition: transform 0.3s;
@@ -526,7 +535,7 @@
                 </button>
                 <p class="response-text">${config.branding.responseTimeText}</p>
             </section>
-            <section style="padding-top: 30px; ">
+            <section style="padding-top: 35px; ">
                 <p>CONNECT WITH US</p>
                 <ul class="new-conversation-socials">
                     
@@ -595,7 +604,7 @@
     if (config.socials.whatsapp != '')
         socialsArea.innerHTML += `
             <li class="social">
-                <a>
+                <a href="${config.socials.whatsapp}" rel="noreferrer" tabindex="0" target="_blank" title="Whatsapp">
                     <span style="display:flex; align-items:center;">
                         <svg class="app-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
                             <path d="M476.9 161.1C435 119.1 379.2 96 319.9 96C197.5 96 97.9 195.6 97.9 318C97.9 357.1 108.1 395.3 127.5 429L96 544L213.7 513.1C246.1 530.8 282.6 540.1 319.8 540.1L319.9 540.1C442.2 540.1 544 440.5 544 318.1C544 258.8 518.8 203.1 476.9 161.1zM319.9 502.7C286.7 502.7 254.2 493.8 225.9 477L219.2 473L149.4 491.3L168 423.2L163.6 416.2C145.1 386.8 135.4 352.9 135.4 318C135.4 216.3 218.2 133.5 320 133.5C369.3 133.5 415.6 152.7 450.4 187.6C485.2 222.5 506.6 268.8 506.5 318.1C506.5 419.9 421.6 502.7 319.9 502.7zM421.1 364.5C415.6 361.7 388.3 348.3 383.2 346.5C378.1 344.6 374.4 343.7 370.7 349.3C367 354.9 356.4 367.3 353.1 371.1C349.9 374.8 346.6 375.3 341.1 372.5C308.5 356.2 287.1 343.4 265.6 306.5C259.9 296.7 271.3 297.4 281.9 276.2C283.7 272.5 282.8 269.3 281.4 266.5C280 263.7 268.9 236.4 264.3 225.3C259.8 214.5 255.2 216 251.8 215.8C248.6 215.6 244.9 215.6 241.2 215.6C237.5 215.6 231.5 217 226.4 222.5C221.3 228.1 207 241.5 207 268.8C207 296.1 226.9 322.5 229.6 326.2C232.4 329.9 268.7 385.9 324.4 410C359.6 425.2 373.4 426.5 391 423.9C401.7 422.3 423.8 410.5 428.4 397.5C433 384.5 433 373.4 431.6 371.1C430.3 368.6 426.6 367.2 421.1 364.5z"/>
@@ -612,7 +621,7 @@
     if (config.socials.email != '')
         socialsArea.innerHTML += `
             <li class="social">
-                <a>
+                <a href="mailto:${config.socials.email}" rel="noreferrer" tabindex="0" target="_blank" title="Email">
                     <span style="display:flex; align-items:center;">
                         <svg class="app-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
                             <path d="M125.4 128C91.5 128 64 155.5 64 189.4C64 190.3 64 191.1 64.1 192L64 192L64 448C64 483.3 92.7 512 128 512L512 512C547.3 512 576 483.3 576 448L576 192L575.9 192C575.9 191.1 576 190.3 576 189.4C576 155.5 548.5 128 514.6 128L125.4 128zM528 256.3L528 448C528 456.8 520.8 464 512 464L128 464C119.2 464 112 456.8 112 448L112 256.3L266.8 373.7C298.2 397.6 341.7 397.6 373.2 373.7L528 256.3zM112 189.4C112 182 118 176 125.4 176L514.6 176C522 176 528 182 528 189.4C528 193.6 526 197.6 522.7 200.1L344.2 335.5C329.9 346.3 310.1 346.3 295.8 335.5L117.3 200.1C114 197.6 112 193.6 112 189.4z"/>
@@ -629,7 +638,7 @@
     if (config.socials.phone != '')
         socialsArea.innerHTML += `
             <li class="social">
-                <a>
+                <a href="tel:${config.socials.phone}" rel="noreferrer" tabindex="0" target="_blank" title="Contact">
                     <span style="display:flex; align-items:center;">
                         <svg class="app-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
                             <path d="M144 288C144 190.8 222.8 112 320 112C417.2 112 496 190.8 496 288L496 332.8C481.9 324.6 465.5 320 448 320L432 320C405.5 320 384 341.5 384 368L384 496C384 522.5 405.5 544 432 544L448 544C501 544 544 501 544 448L544 288C544 164.3 443.7 64 320 64C196.3 64 96 164.3 96 288L96 448C96 501 139 544 192 544L208 544C234.5 544 256 522.5 256 496L256 368C256 341.5 234.5 320 208 320L192 320C174.5 320 158.1 324.7 144 332.8L144 288zM144 416C144 389.5 165.5 368 192 368L208 368L208 496L192 496C165.5 496 144 474.5 144 448L144 416zM496 416L496 448C496 474.5 474.5 496 448 496L432 496L432 368L448 368C474.5 368 496 389.5 496 416z"/>
